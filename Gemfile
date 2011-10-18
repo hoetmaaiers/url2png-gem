e0951c9daecc712cd7d5ac77513a9f6f8adc7c01
Alejandro Andres <fuzzy.alej@gmail.com>
2011-10-18 13:26:01 +0200

    Return local image in test mode to avoid external connection

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
 source "http://rubygems.org"
-# Add dependencies required to use your gem here.
-# Example:
-#   gem "activesupport", ">= 2.3.5"
 
-# Add dependencies to develop your gem here.
-# Include everything needed to run rake, tests, features, etc.
+gem 'png'
+gem 'RubyInline'
+
 group :development do
   gem "rspec", "~> 2.6.0"
   gem "bundler", "~> 1.0.0"
--- lib/url2png/helpers/common.rb
+++ lib/url2png/helpers/common.rb
@@ -1,7 +1,7 @@
 module Url2png
   module Helpers
     module Common
-      
+
       def site_image_tag url, options = {}
         dim = Url2png::Dimensions.parse(options)
 
@@ -20,8 +20,12 @@ module Url2png
 
       def site_image_url url, options = {}
         options[:protocol] ||= 'http://'
-        
         dim = Url2png::Dimensions.parse(options)
+
+        #ONly to be used inside rails app, obviously
+        unless Rails.env.production?
+          return fake_url_for_image_sized dim
+        end
 
         # escape the url
         safe_url = URI.escape(url)
@@ -32,7 +36,29 @@ module Url2png
         # build image url
         File.join(Url2png::Config.api_url(options[:protocol]), Url2png::Config.api_version, Url2png::Config.public_key, token, dim[:size], safe_url)
       end
-      
+
+      private
+
+      def fake_url_for_image_sized dim
+        @image ||= {}
+        size, width, height = dim[:size], dim[:width].to_i, dim[:height].to_i
+
+        if @image.has_key? size
+          @image[size]
+        else
+          path = generate_image_sized width, height
+          @image[size] = path
+        end
+      end
+
+      def generate_image_sized width, height
+        require 'png'
+        png = PNG.new PNG::Canvas.new width, height, PNG::Color::Black
+        name = "u2p-#{rand(36**8).to_s(36)}.png"
+        png.save "#{Rails.root}/app/assets/images/#{name}"
+        name
+      end
+
     end
   end
 end
