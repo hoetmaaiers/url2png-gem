6671fc6489af1df52157737df6db142a944caf6b
Robin Houdmeyers <houdmeyers@gmail.com>
2012-05-24 10:44:46 +0200

    updated config

--- lib/url2png/config.rb
+++ lib/url2png/config.rb
@@ -3,11 +3,19 @@ module Url2png
   # modes
   MODES = %w{production placehold dummy}
   
-  # Setters
+  
+  # SETTERS
   def config c = {}
+    
+    # api_key
+    # private_key
+    # mode        (production[default] placehold dummy)
+    # version     (v6[default])
+    
     c.each do |key, value|
       case key
-      when :api_key        
+        
+      when :api_key   
         @api_key = value
       when :private_key
         @token = value
@@ -20,8 +28,8 @@ module Url2png
     end
   end
   
-  # Getters
-     
+
+  # GETTERS
   def self.api_key
     raise 'Url2png error: No public key defined!' if @url2png_key.nil?
     @url2png_key
@@ -40,35 +48,4 @@ module Url2png
     @url2png_version ||= 'v6' #defaults on v6
   end
   
-  
-  # def self.mode= mode
-  #   raise "Url2png error: Invalid mode, only #{ MODES.join(', ') } are allowed" unless MODES.include?(mode.to_s)
-  #   @url2png_mode ||= mode.to_s
-  # end
-  
-  # api settings
-  # def self.api_url protocol
-  #   @url2png_url ||= "#{ protocol }api.url2png.com"
-  # end
-
-  # shared secret setter and getter
-  # def self.shared_secret= secret
-  #   raise 'Url2png error: Invalid shared secret!' unless secret.match(/^S/)
-  #   @url2png_secret ||= secret
-  # end
-
-  # public key setter and getter
-  # def self.public_key= key
-  #   raise 'Url2png error: Invalid public key!' unless key.match(/^P/)
-  #   @url2png_key ||= key
-  # end
-  # size
-  
-  # def self.default_size= size
-  #   @url2png_size ||= size
-  # end
-
-  # def self.default_size
-  #   @url2png_size ||= '400x400'
-  # end
 end
