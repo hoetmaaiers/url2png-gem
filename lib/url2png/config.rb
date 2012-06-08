b13880d727ca6b30efd1018a3bffc8e5d39c51a8
Pablo Cantero <pablo@pablocantero.com>
2012-06-08 12:16:05 -0300

    Adding tests, adding setters making the README 'valid'

--- lib/url2png/config.rb
+++ lib/url2png/config.rb
@@ -5,38 +5,36 @@ module Url2png
   MODES = %w{production placehold dummy}
   
   def config c = {}
-    # SET
-    #   api_key
-    #   private_key
-    #   mode        (production[default] placehold dummy)
-    #   version     (v6[default])
+    # mandatory
+    self.api_key = c[:api_key]
+    self.private_key = c[:private_key]
     
-    c.each do |key, value|
-      case key
-        
-      when :api_key   
-        @api_key = value
-      when :private_key
-        @private_key = value
-      when :mode
-        raise "Url2png error: Invalid mode, only #{ MODES.join(', ') } are allowed" unless MODES.include?(value.to_s)
-        @mode = value
-      when :api_version
-        @api_version = value
-      end
-    end
+    # optional
+    self.mode = c[:mode] if c[:mode]
+    self.api_version = c[:api_version] if c[:api_version]
   end
   
-
-  # GETTERS
+  def api_key=api_key
+    @api_key=api_key
+  end
+  
   def api_key
     raise 'Url2png error: No public key defined!' if @api_key.nil?
     @api_key
+  end
+  
+  def private_key=private_key
+    @private_key = private_key
   end
 
   def private_key
     raise 'Url2png error: No private key defined!' if @private_key.nil?
     @private_key
+  end
+  
+  def mode=mode
+    raise "Url2png error: Invalid mode, only #{ MODES.join(', ') } are allowed" unless MODES.include?(mode.to_s)
+    @mode = mode
   end
 
   def mode
@@ -44,7 +42,7 @@ module Url2png
   end
   
   def api_version
-    @api_version ||= 'v4' #default: v4
+    @api_version ||= 'v3' #default: v3
   end
   
   def default_size
--- spec/url2png_spec.rb
+++ spec/url2png_spec.rb
@@ -1,7 +1,32 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Url2png" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "attributes" do
+    it "should set mandatory attributes" do
+      my_api_key = "my_api_key"
+      my_private_key = "my_private_key"
+      Url2png.config ({api_key: my_api_key, private_key: my_private_key})
+      Url2png.api_key.should eq my_api_key
+      Url2png.private_key.should eq my_private_key
+    end
+    
+    it "should throw an exception getting nil api_key" do
+      Url2png.config({private_key: "private_key"})
+      expect {Url2png.api_key}.to raise_error
+    end
+    
+    it "should throw an exception getting nil private_key" do
+      Url2png.config({api_key: "private_key"})
+      expect {Url2png.private_key}.to raise_error    end
+    
+    it "should set mode" do
+      mode = "placehold"
+      Url2png.config ({mode: mode})
+      Url2png.mode.should eq mode
+    end
+    
+    it "should throw an exception with an invalid mode" do
+      expect {Url2png.config({mode: "invalid mode"})}.to raise_error
+    end
   end
 end
