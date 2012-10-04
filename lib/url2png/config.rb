73d7e3bffcd44a652f996dabb0fcbd6ff424ff40
Jean-Hadrien Chabran <jh@chabran.fr>
2012-10-04 09:10:29 -0700

    Convert mode to a string in case it's a symbol

--- lib/url2png/config.rb
+++ lib/url2png/config.rb
@@ -1,28 +1,28 @@
 module Url2png
   extend self
-  
+
   # modes
   MODES = %w{production placehold dummy}
-  
+
   def config c = {}
     # mandatory
     self.api_key = c[:api_key]
     self.private_key = c[:private_key]
-    
+
     # optional
     self.mode = c[:mode] if c[:mode]
     self.api_version = c[:api_version] if c[:api_version]
   end
-  
+
   def api_key=api_key
     @api_key=api_key
   end
-  
+
   def api_key
     raise 'Url2png error: No public key defined!' if @api_key.nil?
     @api_key
   end
-  
+
   def private_key=private_key
     @private_key = private_key
   end
@@ -31,32 +31,32 @@ module Url2png
     raise 'Url2png error: No private key defined!' if @private_key.nil?
     @private_key
   end
-  
+
   def mode=mode
     raise "Url2png error: Invalid mode, only #{ MODES.join(', ') } are allowed" unless MODES.include?(mode.to_s)
-    @mode = mode
+    @mode = mode.to_s
   end
 
   def mode
     @mode ||= 'production' # default: production
   end
-  
+
   def api_version=api_version
     @api_version = api_version || 'v6' # set default to latest open version
   end
-  
+
   def api_version
     @api_version || 'v6' #default: v6
   end
-  
+
   def default_size=default_size
     @default_size = default_size || "400x400"
   end
-  
+
   def default_size
     @default_size || "400x400"
   end
-  
+
   def token param
     case self.api_version
     when 'v6'
